8a64023c1643fbcaa556dc82ef11c0aeececc78e
29Shreyas <sihasaneshreyas@gmail.com>
2023-02-03 22:59:36 +0530

    Fifth

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -25,42 +25,42 @@ function Nav({ id, pathname }: any) {
         <div className='flex items-center justify-center max-w-xs md:max-w-md absolute bottom-8 left-0 right-0 mx-auto rounded-lg navbar space-x-6 md:space-x-5 py-4 md:py-2'>
             <a href={'#home'} className='relative group'>
                 <div className='flex items-center flex-col p-0 md:p-2'>
-                    <HiHome className={`w-6 h-6 hover:text-violet-900 ${active ? 'text-violet-900' : 'text-violet-700'}`} />
+                    <HiHome className={`w-6 h-6 group-hover:text-violet-900 ${active ? 'text-violet-900' : 'text-violet-700'}`} />
                     <p className='text-xs text-slate-700 font-normal hidden md:block mt-1 capitalize'>home</p>
                 </div>
                 <Tooltip title='home' />
             </a>
             <a href={'#about'} className='relative group'>
                 <div className='flex items-center flex-col p-0 md:p-2'>
-                    <BsPersonFill className={`w-6 h-6 hover:text-violet-900 ${active ? 'text-violet-900' : 'text-violet-700'}`} />
+                    <BsPersonFill className={`w-6 h-6 group-hover:text-violet-900 ${active ? 'text-violet-900' : 'text-violet-700'}`} />
                     <p className='text-xs text-slate-700 font-normal hidden md:block mt-1 capitalize'>about</p>
                 </div>
                 <Tooltip title='about' />
             </a>
             <a href={'#services'} className='relative group'>
                 <div className='flex items-center flex-col p-0 md:p-2'>
-                    <RiServiceFill className={`w-6 h-6 hover:text-violet-900 ${active ? 'text-violet-900' : 'text-violet-700'}`} />
+                    <RiServiceFill className={`w-6 h-6 group-hover:text-violet-900 ${active ? 'text-violet-900' : 'text-violet-700'}`} />
                     <p className='text-xs text-slate-700 font-normal hidden md:block mt-1 capitalize'>services</p>
                 </div>
                 <Tooltip title='services' />
             </a>
             <a href={'#work'} className='relative group'>
                 <div className='flex items-center flex-col p-0 md:p-2'>
-                    <MdWork className={`w-6 h-6 hover:text-violet-900 ${active ? 'text-violet-900' : 'text-violet-700'}`} />
+                    <MdWork className={`w-6 h-6 group-hover:text-violet-900 ${active ? 'text-violet-900' : 'text-violet-700'}`} />
                     <p className='text-xs text-slate-700 font-normal hidden md:block mt-1 capitalize'>work</p>
                 </div>
                 <Tooltip title='work' />
             </a>
             <a href={'#skills'} className='relative group'>
                 <div className='flex items-center flex-col p-0 md:p-2'>
-                    <FaAward className={`w-6 h-6 hover:text-violet-900 ${active ? 'text-violet-900' : 'text-violet-700'}`} />
+                    <FaAward className={`w-6 h-6 group-hover:text-violet-900 ${active ? 'text-violet-900' : 'text-violet-700'}`} />
                     <p className='text-xs text-slate-700 font-normal hidden md:block mt-1 capitalize'>skills</p>
                 </div>
                 <Tooltip title='skills' />
             </a>
             <a href={'#contact'} className='relative group'>
                 <div className='flex items-center flex-col p-0 md:p-2'>
-                    <IoChatboxEllipses className={`w-6 h-6 hover:text-violet-900 ${active ? 'text-violet-900' : 'text-violet-700'}`} />
+                    <IoChatboxEllipses className={`w-6 h-6 group-hover:text-violet-900 ${active ? 'text-violet-900' : 'text-violet-700'}`} />
                     <p className='text-xs text-slate-700 font-normal hidden md:block mt-1 capitalize'>contact</p>
                 </div>
                 <Tooltip title='contact' />
